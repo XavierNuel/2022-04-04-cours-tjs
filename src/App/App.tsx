cdec4d3025c9954fced0577b25fd4b4183a08b90
XavierNuel <xavier@net-concept.fr>
2022-04-04 14:23:56 +0200

    Mise en place d'un evt

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
-      Test
+      Test<br />
+      <Button text="Press the button" 
+      evtOnClick={ (arg: any) => {
+        console.log(arg);
+      }} />
+      <Button text="Don't push the ref button" bgColor="tomato" />
+      <Button text="Benjamin button"/>
     </div>
   );
 }
