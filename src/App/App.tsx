a0da8315c525460865a7df53255bff0fbe0de6b6
XavierNuel <xavier@net-concept.fr>
2022-04-04 15:02:53 +0200

    Ajout de styles 'spread' dans un autre object...

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import './App.css';
-import Button from './components/Button/Button';
+import React from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
-      Test<br />
+      Test
+      <br />
       <Button
-      evtOnClick={ (arg: any) => {
-        console.log(arg);
-      }}>
+        evtOnClick={(arg: any) => {
+          console.log(arg);
+        }}
+        color="#0ff"
+        type="submit"
+        style={{border: '2px solid #f00'}}
+      >
         <div>Enfant 1</div>
         <div>Enfant 2</div>
       </Button>
       <Button bgColor="tomato">Chaine</Button>
-      <Button>
+      <Button bgColor="#000" color="#ff0" type="reset">
         <div>Enfant seul</div>
       </Button>
     </div>
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,25 +1,39 @@
-import React from 'react'
-import style from './Button.module.css';
-import PropTypes from 'prop-types'
+import React from "react";
+import style from "./Button.module.css";
+import PropTypes from "prop-types";
+import { isPropertyAccessOrQualifiedName } from "typescript";
 
 function Button(props) {
-    console.log(props);
-    return (
-        <button className={ style.Button } onClick={(evt)=> {
-            props.evtOnClick('Hello');
-            }}
-            style={ {backgroundColor: props.bgColor } }
-        >{props.children}</button>
-    )
+  console.log(props);
+  return (
+    <button
+      className={style.Button}
+      onClick={(evt) => {
+        props.evtOnClick("Hello");
+      }}
+      style={{...props.style, backgroundColor: props.bgColor, color: props.color }}
+      type={props.type}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 // On défini les propriétés du bouton et le type, et si c'est requis
-Button.propTypes={
-    evtOnClick :  PropTypes.func.isRequired,
-    bgColor: PropTypes.string,
-    children: PropTypes.any.isRequired,
-}
-Button.defaultProps= {
-    evtOnClick :  ()=>{console.error('Fonction du button undefined /App/Button')},
-}
-export default Button
+Button.propTypes = {
+  evtOnClick: PropTypes.func.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+  type: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  style: PropTypes.object,
+};
+Button.defaultProps = {
+  evtOnClick: () => {
+    console.error("Fonction du button undefined /App/Button");
+  },
+  type: "button",
+  color: "#fff",
+  bgColor: "rgb(0, 54, 85)",
+};
+export default Button;
