45de6faf3c24dddc654eb6db71f1fcfe20cabe5a
XavierNuel <xavier@net-concept.fr>
2022-04-05 15:10:21 +0200

    Ajout d'images supplémentaires

---
+++ src/App/store/store.js
@@ -0,0 +1,11 @@
+// Pour passer en TS, il faudrait ajouter des interfaces
+function ressourceReducer(state, action) {
+
+    switch(action.type) {
+
+
+        
+
+        default: return state;
+    }
+}
